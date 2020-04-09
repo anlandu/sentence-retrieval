a5fe915fd1b228c16b4c18514a5026689c6517f4
aidansan <aidan.w.san@gmail.com>
2020-04-09 18:55:38 -0400

    flaskserver calls retrieval system

--- flaskserver.py
+++ flaskserver.py
@@ -1,19 +1,19 @@
 #https://flask.palletsprojects.com/en/1.1.x/quickstart/#apis-with-json
 from flask import Flask, request
-
+from retrieval.sentence_index import search_index
 
 
 app = Flask(__name__)
 
-def query_recommendations(q):
-    return [''.join(reversed(q))]
+def query_recommendations(q, n=10):
+    return [h['content'] for h in list(search_index(q, 'retrieval/sentence_index'))[:n]]
 
 @app.route('/recommendations', methods=['POST', 'GET'])
 def recommendations():
     query="default"
     if request.method == 'POST':
         query = request.form['query']
-        #add a party for number of recommendations to ask for
+        #add a part for number of recommendations to ask for
         print(request.form)
     # the code below is executed if the request method
     return {
