32255fcb4ef7c135471d04c0a686487911c62614
Aidan San <muakasan@gmail.com>
2020-04-24 17:51:17 -0400

    added code to fetch release to flaskserver

--- flaskserver.py
+++ flaskserver.py
@@ -6,17 +6,16 @@ import os
 INDEX_DIR = 'retrieval/sentence_index'
 DATA_DIR = 'data/scisummnet'
 
-# def initialize():
-#     if not os.path.exists(DATA_DIR):
-#         #https://stackoverflow.com/questions/3777301/how-to-call-a-shell-script-from-python-code/3777308
-#         print('DOWNLOADING SCISUMMNET')
-#         os.system('sh ./data/download_data.sh')
-#     if not os.path.exists(INDEX_DIR):
-#         os.mkdir(INDEX_DIR)
-#         print('BUILDING INDEX AT "{0}"'.format(INDEX_DIR))
-#         make_clean_index(INDEX_DIR)
-
-# initialize()
+DOWNLOAD_INDEX = True
+def initialize():
+    if not os.path.exists(INDEX_DIR):
+        if DOWNLOAD_INDEX:
+            os.system('sh ./retrieval/download_index.sh')
+        else:
+            # Add code to manually build index
+            pass
+        
+initialize()
 app = Flask(__name__)
 CORS(app)
 
