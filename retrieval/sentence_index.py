340a17660d1fe1eb747409142f0f7037ae798aee
Sanxing Chen <mestayinloves@gmail.com>
2020-05-07 01:34:23 -0400

    Limit the length of sentence between 30 and 120

--- retrieval/sentence_index.py
+++ retrieval/sentence_index.py
@@ -1,16 +1,18 @@
-import os.path
+import xml.etree.ElementTree as ET
+from os import listdir, mkdir
+import shutil
+
+
 from whoosh import index
 from whoosh.fields import Schema, ID, TEXT
 from whoosh.qparser import QueryParser
 from whoosh.qparser import FuzzyTermPlugin
 from whoosh.qparser import OrGroup
 from whoosh.scoring import BM25F, TF_IDF, Frequency
+from whoosh.writing import AsyncWriter
 from whoosh import analysis
 
 from .scoring import OkBM25, PLN
-
-import xml.etree.ElementTree as ET
-from os import listdir
 
 ROOT_PATH = 'data/scisummnet/top1000_complete/'
 
@@ -21,6 +23,9 @@ def get_doc_ids():
 
 
 def make_clean_index(dirname):
+    if index.exists_in(dirname):
+        shutil.rmtree(dirname)
+    mkdir(dirname)
     # Always create the index from scratch
     ix = index.create_in(dirname, schema=get_schema())
     writer = ix.writer()
@@ -28,8 +33,7 @@ def make_clean_index(dirname):
     # Assume we have a function that gathers the filenames of the
     # documents to be indexed
     for path in get_doc_ids():
-        add_doc(writer, path)
-
+        add_doc(path, writer)
     writer.commit()
 
 
@@ -38,7 +42,7 @@ def get_schema():
                   content=TEXT(stored=True, analyzer=analysis.StemmingAnalyzer()))
 
 
-def add_doc(writer, id):
+def add_doc(id, writer):
     path = ROOT_PATH + id
     doc_tree = ET.parse(path + '/Documents_xml/' + id + '.xml')
     doc_root = doc_tree.getroot()
@@ -47,8 +51,9 @@ def add_doc(writer, id):
     for section in doc_root:
         # print(section.tag, section.attrib)
         for sent in section:
-            writer.add_document(id=str(id)+str(i), content=sent.text)
-            i += 1
+            if 30 < len(sent.text) < 120:
+                writer.add_document(id=str(id)+str(i), content=sent.text)
+                i += 1
     #summary_tree = ET.parse(path + '/summary/' + id + '.gold.txt')
     #summary_root = summary_tree.getroot()
 
@@ -81,7 +86,7 @@ def main():
     ix_dir = 'retrieval/sentence_index'
     # make_clean_index(ix_dir)
     r = search_index(
-        "Comparisons against related algorithms are also conducted.", ix_dir)
+        "Comparisons against related algorithms are also conducted.", 'ok', ix_dir)
     print(len(r))
     for result in r[:10]:
         print(result.highlights("content"))
